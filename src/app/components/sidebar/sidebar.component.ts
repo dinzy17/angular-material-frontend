f0745da5ffba08c109fc98a6b00657116fd88bb3
Dinesh <dineshnarendrapatil@gmail.com>
2019-11-12 20:30:22 +0530

    initial commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core'
+
+declare interface RouteInfo {
+    path: string
+    title: string
+    icon: string
+    class: string
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+]
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[]
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem)
+  }
+
+  //function to check if it is mobile menu
+  isMobileMenu() {
+      if (window.screen.width > 991) {
+          return false
+      }
+      return true
+  }
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+  
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
